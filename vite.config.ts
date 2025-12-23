03b415f6abf210636b7db6d183d71e3f5eb0357e
Reina630 <89133502+Reina630@users.noreply.github.com>
2025-12-23 19:54:14 +0100

    Fix base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Kokowa-2025/',
+  base: '/kokowa2025/',
   server: {
     host: "::",
     port: 8080,
